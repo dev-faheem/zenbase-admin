037ed6d96dcb25e8a101581996a1f682a7d84fad
Sampan Verma <sampan.developer@gmail.com>
2022-05-19 04:24:59 +0530

    Updated to current server config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  api: "http://18.196.187.86:4000/admin",
-  app: "http://18.196.187.86",
+  api: "http://3.123.230.25:4000/admin",
+  app: "http://3.123.230.25",
 };
 export default config;
