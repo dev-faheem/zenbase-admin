b36b0c599d51e27296a2b50573788b8274c365ab
Dev Faheem <95623464+dev-faheem@users.noreply.github.com>
2023-06-27 05:13:32 -0700

    delete build item

--- src/pages/auth/logout.js
+++ src/pages/auth/logout.js
@@ -10,6 +10,7 @@ export default function Logout() {
   useEffect(() => {
     deleteUser();
     history.push("/login");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
